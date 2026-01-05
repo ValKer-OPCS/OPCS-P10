ee14f71420c323dab49ec2324a2ebe51ed93acf7
ValKer-OPCS <icyfry@gmail.com>
2026-01-05 10:41:43 +0100

    Refactor app structure and add Redux store setup
    Replaces Vite/React demo content with routing for Home, SignIn, Account, and Error pages. Updates index.html with new title, language, and Font Awesome. Removes unused SVG assets. Adds Redux Toolkit store and user reducer, wrapping the app in a Provider. Expands App.css with new styles for navigation, layout, and components.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from 'react-redux';
+
+import userReducer  from './Stores/userStore.ts';
+
+const store = configureStore({
+
+  reducer: { user:userReducer },
+
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+
+    <Provider store={store}>
     <App />
+    </Provider>
+
+
   </StrictMode>,
 )
