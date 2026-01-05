b8d61a0e2b801ebb8f9ac7f2991310c48c749789
ValKer-OPCS <icyfry@gmail.com>
2026-01-05 11:08:52 +0100

    Add Footer component with styles
    Introduced a new Footer component in the application along with its SCSS module for styling. The footer displays a copyright notice and is styled to be centered with a top border.

---
+++ src/Containers/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import styles from './style.module.scss'
+
+
+
+const Footer = () => {
+  return (
+    <footer className={styles["footer"]}>
+			<p className={styles["footer-text"]}>Copyright 2020 Argent Bank</p>
+		</footer>
+  )
+};
+
+export default Footer
