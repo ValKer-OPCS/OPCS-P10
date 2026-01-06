3150112a068be548a52e686b01b4c3feb8f613a0
ValKer-OPCS <icyfry@gmail.com>
2026-01-06 10:06:45 +0100

    Refactor Home page to use Banner component
    Replaced the hero section in Home.tsx with the reusable Banner component, providing responsive images for better performance. Removed related background-image styles from style.module.scss as the Banner now handles image display.

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,19 +1,35 @@
 import styles from './style.module.scss'
 import FeatureCard from "../../Components/FeatureCard/FeatureCard";
+import Banner from "../../Components/Banner/Banner";
+
+import bannerImg400 from '../../Assets/bank-tree-400w.webp'
+import bannerImg800 from '../../Assets/bank-tree-800w.webp'
+import bannerImg1200 from '../../Assets/bank-tree-1200w.webp'
+import bannerImg from '../../Assets/bank-tree.webp'
 
 
 const Home = () => {
   return (
     <main>
-    <div className={styles["hero"]}>
-				<section className={styles["hero-content"]}>
+     <Banner  src={bannerImg}
+            images={[
+              { src: bannerImg400, width: 400 },
+              { src: bannerImg800, width: 800 },
+              { src: bannerImg1200, width: 1200 },]}
+            sizes="(max-width: 400px) 400px,
+                  (max-width: 800px) 800px,
+                  1200px"
+            alt="photo d'une plante dans un verre rempli de pièces de monnaie">
+
+        <section className={styles["hero-content"]}>
 					<h2 className={styles["sr-only"]}>Promoted Content</h2>
 					<p className={styles["subtitle"]}>No fees.</p>
 					<p className={styles["subtitle"]}>No minimum deposit.</p>
 					<p className={styles["subtitle"]}>High interest rates.</p>
 					<p className={styles["text"]}>Open a savings account with Argent Bank today!</p>
 				</section>
-			</div>
+
+   </Banner>
       <section className={styles["features"]}>
 				<h2 className={styles["sr-only"]}>Features</h2>
 				<FeatureCard icon="chat" title="You are our #1 priority" text="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes." />
