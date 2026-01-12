8488f8218765680ad78a8b72ef36fb4d03ac5a68
ValKer-OPCS <icyfry@gmail.com>
2026-01-12 10:21:49 +0100

    Add user store with login reducer
    Introduces a Redux slice for user state management, including an initial state and a login reducer to update user information.

---
+++ src/Stores/userStore.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    value: {
+      firstName: "",
+      lastName: "",
+      userName: "",
+      email: "",
+    },
+  },
+  reducers: {
+    login: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export default userSlice.reducer;
