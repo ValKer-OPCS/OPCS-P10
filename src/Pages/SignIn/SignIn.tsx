73b52c03d02233faf63754bc65e2806675e6a161
ValKer-OPCS <icyfry@gmail.com>
2026-01-12 09:54:21 +0100

    Refactor SignIn page to use Form component
    Replaced the inline sign-in form in SignIn.tsx with a reusable Form component for better modularity. Removed related form styles from style.module.scss as they are now handled by the Form component.

--- src/Pages/SignIn/SignIn.tsx
+++ src/Pages/SignIn/SignIn.tsx
@@ -1,6 +1,7 @@
 
 import styles from './style.module.scss'
 
+import Form from '../../Containers/Form/Form';
 
 
 const SignIn = () => {
@@ -9,24 +10,8 @@ const SignIn = () => {
 			<section className={styles["sign-in-content"]}>
 				<i className="fa fa-user-circle {styles[sign-in-icon]}"></i>
 				<h1>Sign In</h1>
-				<form>
-					<div className={styles["input-wrapper"]}>
-						<label htmlFor="username">Username</label>
-						<input type="text" id="username" />
-					</div>
-					<div className={styles["input-wrapper"]}>
-						<label htmlFor="password">Password</label>
-						<input type="password" id="password"  />
-					</div>
-					<div className={styles["input-remember"]}>
-						<input type="checkbox" id="remember-me"  />
-						<label htmlFor="remember-me">Remember me</label>
-					</div>
-
-					<button type="submit" className={styles["sign-in-button"]}>
-						Sign In
-					</button>
-				</form>
+			
+			<Form/>
 			</section>
 		</main>
   )
