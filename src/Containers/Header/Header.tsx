d11e4355de1eac62244e1a7428a659e96932129f
ValKer-OPCS <icyfry@gmail.com>
2026-01-14 10:27:28 +0100

    Replace div with nav for navigation links in Header
    Updated the Header component to use a semantic <nav> element instead of a <div> for the navigation links, improving accessibility and HTML structure.

--- src/Containers/Header/Header.tsx
+++ src/Containers/Header/Header.tsx
@@ -8,12 +8,12 @@ const Header = () => {
   return (
     <header className={styles["main-nav"]}>
       <Logo/>
-      <div>
+      <nav>
         <NavLink className={styles["main-nav-item"]} to="/signIn"> 
           <i className="fa fa-user-circle"></i>
           &nbsp;Sign In
         </NavLink>
-      </div>
+      </nav>
     </header>
   )
 };
