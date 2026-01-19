daa086ca3cb12f08dff99aad4dfbee37980f85e7
ValKer-OPCS <icyfry@gmail.com>
2026-01-19 10:19:22 +0100

    Add user authentication and sign out to Header
    Header now displays user first name and a sign out option when authenticated, using Redux state. Added styling for user navigation section.

--- src/Containers/Header/Header.tsx
+++ src/Containers/Header/Header.tsx
@@ -1,21 +1,54 @@
 import Logo from "../../Components/Logo/Logo";
 import { NavLink } from "react-router-dom";
+import type { AppDispatch, RootState } from "../../main";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../Redux/authSlice";
 
 
 import styles from './style.module.scss'
 
 const Header = () => {
+
+  const dispatch = useDispatch<AppDispatch>()
+
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated)
+  const { user } = useSelector((state: RootState) => state.user)
+  const firstName = user?.firstName
+
+  const handlelogout = () => {
+      dispatch(logout())
+
+  }
+
+
+
+
   return (
     <header className={styles["main-nav"]}>
-      <Logo/>
+      <Logo />
       <nav>
-        <NavLink className={styles["main-nav-item"]} to="/signIn"> 
-          <i className="fa fa-user-circle"></i>
-          &nbsp;Sign In
-        </NavLink>
+        {!isAuthenticated || !firstName ? (
+          <NavLink className={styles["main-nav-item"]} to="/signIn" onClick={handlelogout}>
+            <i className="fa fa-user-circle"></i>
+            &nbsp;Sign In
+          </NavLink>
+        ) : (
+          <div className={styles["main-nav-user"]}>
+            <NavLink to="/account" className={styles["main-nav-item"]}>
+              <i className="fa fa-user-circle"></i>
+              &nbsp;{firstName}&nbsp;
+            </NavLink>
+            
+
+            <NavLink to="/" className={styles["main-nav-item"]} onClick={handlelogout}>
+            <i className="fa fa-sign-out"></i>
+              &nbsp;Sign Out
+            </NavLink>
+          </div>
+        )}
       </nav>
     </header>
   )
-};
+}
 
 export default Header
