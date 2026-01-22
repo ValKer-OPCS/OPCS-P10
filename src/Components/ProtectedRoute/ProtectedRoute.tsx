113fa8cd96ee40e5c1d0ce39c121b40f95ef478d
ValKer-OPCS <icyfry@gmail.com>
2026-01-22 19:29:31 +0100

    Refactor ProtectedRoute to arrow function
    Converted ProtectedRoute from a function declaration to an arrow function for consistency and improved readability.

--- src/Components/ProtectedRoute/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute/ProtectedRoute.tsx
@@ -2,12 +2,14 @@ import type { RootState } from "../../main";
 import { Navigate, Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-export default function ProtectedRoute() {
- const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated)
+const ProtectedRoute = () => {
 
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated)
   if (!isAuthenticated) {
     return <Navigate to="/signin" replace />
   }
 
   return <Outlet />
-}
+  };
+
+export default ProtectedRoute
