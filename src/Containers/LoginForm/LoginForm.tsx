8feb0676ce4b2042c7aa295da57950afbf399cdd
ValKer-OPCS <icyfry@gmail.com>
2026-01-23 10:31:41 +0100

    Display login error message in LoginForm
    Added error handling to the LoginForm component to display authentication errors below the form. Introduced a new SCSS module for styling the error message.

--- src/Containers/LoginForm/LoginForm.tsx
+++ src/Containers/LoginForm/LoginForm.tsx
@@ -8,6 +8,8 @@ import Button from '../../Components/Button/Button';
 import Checkbox from '../../Components/Checkbox/Checkbox';
 import FormTextField from '../../Components/FormTextField/FormTextField';
 
+import styles from './style.module.scss'
+
 const LoginForm = () => {
   const dispatch = useDispatch<AppDispatch>()
   const navigate = useNavigate()
@@ -16,17 +18,19 @@ const LoginForm = () => {
   const userLoading = useSelector((state: RootState) => state.user.loading)
   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated)
 
+  const { error } = useSelector((state: RootState) => state.auth)
+
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [rememberMe, setRememberMe] = useState(false)
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    dispatch(login({ email, password, rememberMe  }))
+    dispatch(login({ email, password, rememberMe }))
   }
 
   useEffect(() => {
-    if (isAuthenticated ) {
+    if (isAuthenticated) {
       navigate('/account', { replace: true })
     }
   }, [isAuthenticated, navigate])
@@ -34,15 +38,21 @@ const LoginForm = () => {
   const loading = authLoading || userLoading
 
   return (
+    <>
     <form onSubmit={handleSubmit}>
       <FormTextField id="username" variant="input-wrapper" text="Username" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
 
-      <FormTextField id="password" variant="input-wrapper" text="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+      <FormTextField id="password" variant="input-wrapper" text="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
       <Checkbox id="remember-me" variant="input-remember" text="Remember me" checked={rememberMe} onChange={setRememberMe} />
 
       <Button type="submit" variant="sign-in-button" text={loading ? 'Loading...' : 'Sign In'} disabled={loading} />
     </form>
+
+    {
+    error && ( <p className={styles.formError}>{error}</p> )
+  }
+  </>
   );
 };
 
